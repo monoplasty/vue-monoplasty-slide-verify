f7317e4bd49c113052e157e412255c1754684b26
yuqian <yuq@zhejianglab.com>
2022-07-07 17:02:23 +0800

    feat: 事件增加节流

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,59 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
-  // entry: './src/lib/index.js',
-  entry: './src/main.js',
+  entry: "./src/lib/index.js",
+  // entry: "./src/main.js",
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'slide-verify.js',
-    library: 'SlideVerify',
-    libraryTarget: 'umd',
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/dist/",
+    filename: "slide-verify.js",
+    library: "SlideVerify",
+    libraryTarget: "umd",
     umdNamedDefine: true
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ],
-      },      {
+        use: ["vue-style-loader", "css-loader"]
+      },
+      {
         test: /\.vue$/,
-        loader: 'vue-loader',
+        loader: "vue-loader",
         options: {
-          loaders: {
-          }
+          loaders: {}
           // other vue-loader options go here
         }
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
         use: [
-         { loader: 'url-loader',
-        options: {
-          limit: '8192',
-          name: 'imgs/[name].[hash].[ext]',
-          publicPath: './dist/'
-        }}
-        ],
-
+          {
+            loader: "url-loader",
+            options: {
+              limit: "8192",
+              name: "imgs/[name].[hash].[ext]",
+              publicPath: "./dist/"
+            }
+          }
+        ]
       }
     ]
   },
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      vue$: "vue/dist/vue.esm.js"
     },
-    extensions: ['*', '.js', '.vue', '.json']
+    extensions: ["*", ".js", ".vue", ".json"]
   },
   devServer: {
-    host: 'localhost',
+    host: "localhost",
     historyApiFallback: true,
     noInfo: true,
     overlay: true
@@ -63,15 +61,15 @@ module.exports = {
   performance: {
     hints: false
   },
-  devtool: '#eval-source-map'
-}
+  devtool: "#eval-source-map"
+};
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+if (process.env.NODE_ENV === "production") {
+  module.exports.devtool = "#source-map";
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
-      'process.env': {
+      "process.env": {
         NODE_ENV: '"production"'
       }
     }),
@@ -84,5 +82,5 @@ if (process.env.NODE_ENV === 'production') {
     new webpack.LoaderOptionsPlugin({
       minimize: true
     })
-  ])
+  ]);
 }
